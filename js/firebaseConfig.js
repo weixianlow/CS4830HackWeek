d2e64315acafb4993c78c514fc27766b07498417
Wei Xian Low <email@weixianlow.me>
2017-11-20 16:46:05 -0600

    Added Universal config file.

---
+++ js/firebaseConfig.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyBoJ2xUGe_oX9llU1BG8muaGzm5hI9JruQ",
+    authDomain: "cs4830hackweek.firebaseapp.com",
+    databaseURL: "https://cs4830hackweek.firebaseio.com",
+    projectId: "cs4830hackweek",
+    storageBucket: "cs4830hackweek.appspot.com",
+    messagingSenderId: "430410065620"
+  };
+  firebase.initializeApp(config);
