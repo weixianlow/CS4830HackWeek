e12234af11686adaedb5370fd9558c6d11151822
Wei Xian Low <email@weixianlow.me>
2017-11-20 20:09:42 -0600

    added init for firechat

--- js/app.js
+++ js/app.js
@@ -3,8 +3,19 @@ var userInfo = undefined;
 firebase.auth().onAuthStateChanged(function(user){
 	if(user){
 		userInfo = user;
+
+		initChat(user);
 	} else {
 		//redirect to login page if user hasn't logon
 		window.location.replace("http://cs4085.weixianlow.me/login.html");
 	}
 });
+
+function initChat(user){
+	var chatRef = firebase.database().ref("chat");
+
+	var chat = new Firechat(chatRef);
+	chat.setUser(user.uid, user.displayName, function(user){
+
+	})
+}
