aafbd058109283a04ac79b81f76e9c2bff3ce21b
Wei Xian Low <email@weixianlow.me>
2017-11-20 20:13:14 -0600

    Added create room functionality (test)

--- js/groups.js
+++ js/groups.js
@@ -2,7 +2,11 @@ angular.module('ChatGroupList', []).controller('GroupListController', function()
 	this.create = function(){
 		if(this.newGroupName){
 			console.log(this.newGroupName);
-			this.newGroupName = "";
+			chat.createRoom(this.newGroupName, "public", roomCreated(roomId));
 		}
 	}
 });
+
+this.roomCreated = function(roomId){
+	console.log(roomId);
+}
