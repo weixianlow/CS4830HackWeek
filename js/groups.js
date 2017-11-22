90b5016ed432a5c5b76a78278c3e3f72be9fa58d
FireLemons <ShenYang64@gmail.com>
2017-11-22 17:47:46 -0600

    user accoutn creation partaily working

--- js/groups.js
+++ js/groups.js
@@ -1,16 +1,26 @@
+dbRef.child("users/" + userInfo.uid).once("value").then(function(snapshot){
+	if(!(snapshot.val() && snapshot.val().username)){
+		dbRef.child("users/" + userInfo.uid).set({
+			username: userInfo.displayName,
+			email: userInfo.email,
+			uid : userInfo.uid
+		});
+	}
+});
+
 angular.module('ChatGroupList', []).controller('GroupListController', function($scope){
 
 	this.loadGroups = function(){
 
 		var outer = this;
 
-		chat.getRoomList(function(groupObj){
+		/*chat.getRoomList(function(groupObj){
 			outer.groupList = Object.entries(groupObj).map(function(elem){
 				return elem[1];
 			});
 
 			$scope.$apply();
-		});
+		});*/
 	}
 
 	this.create = function(){
