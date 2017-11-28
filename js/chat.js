167f886e620a3b3fc8980fdd146927e228db4f59
FireLemons <ShenYang64@gmail.com>
2017-11-28 03:06:20 -0600

    chat nearly functional

--- js/chat.js
+++ js/chat.js
@@ -5,9 +5,17 @@ angular.module('Chat', []).controller('ChatController', function($scope, $window
 
 	this.messages = [];	
 
-	this.loadMessages = function(){
-		 dbRef.child("groups/" + groupName + "/messages/").once('value').limitToLast(50).orderByValue().then();
-	}
+	/*this.loadMessages = function(){
+		 dbRef.child("groups/" + groupName + "/messages/").limitToLast(50).once('value').then(function(snapshot){
+			outer.messages = Object.entries(snapshot.val()).map(function(elem){
+				return {
+					name: elem[1].owner,
+					data: elem[1].data
+				};
+			});
+			$scope.$apply();
+		});
+	}*/
 
 	this.sendMessage = function(){
 		if(outer.input){
@@ -21,5 +29,16 @@ angular.module('Chat', []).controller('ChatController', function($scope, $window
 
 			outer.input = "";
 		}
-	} 
+	}
+
+	dbRef.child("groups/" + groupName + "/messages/").on("child_added", function(snapshot){
+		var newMessage = snapshot.val();
+
+		outer.messages.push({
+			name: newMessage.owner,
+			data: newMessage.data
+		});
+
+		//$scope.$apply();
+	});
 });
